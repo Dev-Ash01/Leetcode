e2d3a5b3aca0072213c0833e0e4601dc3293d11a
Ayush Patil <169033196+Dev-Ash01@users.noreply.github.com>
2025-12-21 20:04:08 +0530

    Time: 50 ms (75.81%) | Memory: 18 MB (99.12%) - LeetSync

--- 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.py
@@ -1,26 +1,11 @@
-import numpy as np 
-
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        interList = []
-
-        for word in strs:
-            interList.append(list(word))
-        
-        interList = np.array(interList)
-        interList = interList.transpose()
-
-        count = 0
-
-        for row in interList:
-
-            i = 0
-            while(i <= len(row) - 2):
-
-                if(row[i] > row[i + 1]):
-                    count += 1
-                    break
-
-                i += 1
-
-        return count      
+        cnt = 0
+        n = len(strs[0])
+        for i in range(n):
+            res = []
+            for j in strs:
+                res.append(j[i])
+            if res != sorted(res):
+                cnt += 1
+        return cnt
