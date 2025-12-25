1896da7258289b2112f81eb141e79c57e3d5e2ad
Ayush Patil <169033196+Dev-Ash01@users.noreply.github.com>
2025-12-25 23:17:31 +0530

    Time: 822 ms (60.28%) | Memory: 44.1 MB (65.25%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.py
@@ -1,13 +1,11 @@
 class Solution:
-    def maximumHappinessSum(self, christmasJoy: List[int], gifts: int) -> int:
-        # Santa gives gifts to the happiest children first
-        christmasJoy.sort(reverse=True)
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        ans = 0
+        happiness.sort(reverse = True)
+        i = 0
+        while(i < k):
+
+            ans += max(0, happiness[i] - i)
+            i += 1
         
-        totalJoy = 0
-        for turn in range(gifts):
-            currentJoy = christmasJoy[turn] - turn
-            if currentJoy <= 0:
-                break
-            totalJoy += currentJoy
-        
-        return totalJoy
+        return ans
