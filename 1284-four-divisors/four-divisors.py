e51549d46e7a239fdaf65b3eeca5e518cd2e0e81
Ayush Patil <169033196+Dev-Ash01@users.noreply.github.com>
2026-01-04 10:49:32 +0530

    Time: 186 ms (66.90%) | Memory: 18.5 MB (98.61%) - LeetSync

--- 1284-four-divisors/four-divisors.py
+++ 1284-four-divisors/four-divisors.py
@@ -1,29 +1,21 @@
 class Solution:
-    def sumFourDivisors(self, nums: list[int]) -> int:
-        res = 0
-        for n in nums:
-            val = self.sumOne(n)
-            if val != -1:
-                res += val
-        return res
-
-    def sumOne(self, n: int) -> int:
-        p = round(n ** (1/3))
-        if p ** 3 == n and self.isPrime(p):
-            return 1 + p + p*p + p*p*p
-
-        for i in range(2, int(n ** 0.5) + 1):
-            if n % i == 0:
-                a, b = i, n // i
-                if a != b and self.isPrime(a) and self.isPrime(b):
-                    return 1 + a + b + n
-                return -1
-        return -1
-
-    def isPrime(self, x: int) -> bool:
-        if x < 2:
-            return False
-        for i in range(2, int(x ** 0.5) + 1):
-            if x % i == 0:
-                return False
-        return True
+    def sumFourDivisors(self, nums: List[int]) -> int:
+        total_sum = 0
+        
+        for num in nums:
+            found_divisor = 0
+            divisor = 2
+            
+            while divisor * divisor <= num:
+                if num % divisor == 0:
+                    if found_divisor == 0:
+                        found_divisor = divisor
+                    else:
+                        found_divisor = 0
+                        break
+                divisor += 1
+            
+            if found_divisor > 0 and found_divisor != num // found_divisor:
+                total_sum += 1 + num + found_divisor + num // found_divisor
+        
+        return total_sum
