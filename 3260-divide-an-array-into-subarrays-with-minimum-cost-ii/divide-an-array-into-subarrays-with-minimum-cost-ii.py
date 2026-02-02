7d0a59d93d29fadb3e1b515abfc74e0a1da23d4d
Ayush Patil <169033196+Dev-Ash01@users.noreply.github.com>
2026-02-02 21:29:18 +0530

    Time: 579 ms (100.00%) | Memory: 45.3 MB (8.70%) - LeetSync

--- 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ 3260-divide-an-array-into-subarrays-with-minimum-cost-ii/divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,59 +1,52 @@
-class Container:
-    def __init__(self, k: int):
-        self.k = k
-        self.st1 = SortedList()
-        self.st2 = SortedList()
-        self.sm = 0
-
-    def adjust(self):
-        while len(self.st1) < self.k and len(self.st2) > 0:
-            x = self.st2[0]
-            self.st1.add(x)
-            self.st2.remove(x)
-            self.sm += x
-
-        while len(self.st1) > self.k:
-            x = self.st1[-1]
-            self.st2.add(x)
-            self.st1.remove(x)
-            self.sm -= x
-
-    # insert element x
-    def add(self, x: int):
-        if len(self.st2) > 0 and x >= self.st2[0]:
-            self.st2.add(x)
-        else:
-            self.st1.add(x)
-            self.sm += x
-        self.adjust()
-
-    # delete element x
-    def erase(self, x: int):
-        if x in self.st1:
-            self.st1.remove(x)
-            self.sm -= x
-        elif x in self.st2:
-            self.st2.remove(x)
-        self.adjust()
-
-    # sum of the first k smallest elements
-    def sum(self) -> int:
-        return self.sm
-
-
 class Solution:
     def minimumCost(self, nums: List[int], k: int, dist: int) -> int:
-        n = len(nums)
-        cnt = Container(k - 2)
-        for i in range(1, k - 1):
-            cnt.add(nums[i])
+        heap_used, heap_unused, used = [], [], set()
+        s, ans = 0, inf
+        for right in range(1, len(nums)):
+            left = right - dist - 1
 
-        ans = cnt.sum() + nums[k - 1]
-        for i in range(k, n):
-            j = i - dist - 1
-            if j > 0:
-                cnt.erase(nums[j])
-            cnt.add(nums[i - 1])
-            ans = min(ans, cnt.sum() + nums[i])
+            # Move the left border of the window
+            if left > 0 and left in used:
+                    # If the left element was used in calculating the cost, then we delete it
+                used.remove(left)
+                s -= nums[left]
 
-        return ans + nums[0]
+                    # Find the smallest unused element within the window boundary
+                while heap_unused and heap_unused[0][1] < left:
+                    heappop(heap_unused)
+
+                if heap_unused: # If it exists, use it to calculate the cost
+                    num, i = heappop(heap_unused)
+                    heappush(heap_used, (-num, i))
+                    used.add(i)
+                    s += num
+
+            # Move the right border of the window
+            if len(used) < k - 1:
+                # If less than k-1 elements are used, use the added element to calculate the cost
+                heappush(heap_used, (-nums[right], right))
+                used.add(right)
+                s += nums[right]
+            else:
+                    #Find the largest used element
+                while heap_used[0][1] not in used:
+                    heappop(heap_used)
+                
+                if nums[right] < -heap_used[0][0]:
+                        # If it is larger than the element being added to the window, replace it
+                    num, i = heapreplace(heap_used, (-nums[right], right))
+                    used.remove(i)
+                    used.add(right)
+                    s += num + nums[right]
+
+                        # Add the vacated element to the unused element heap
+                    heappush(heap_unused, (-num, i))
+                else:
+                    # If the element being added to the window is larger than the largest used element, 
+                    # place it on the unused element heap
+                    heappush(heap_unused, (nums[right], right))
+
+            if left >= 0:
+                ans = min(s, ans)
+
+        return nums[0] + ans
